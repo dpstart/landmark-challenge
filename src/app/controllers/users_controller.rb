2b04215d44620e1c5330f9c029b50fc0e02ffd02
daniele-cappuccio <daniele97bsk@gmail.com>
2018-05-14 02:58:23 +0200

    UsersControllers logic implemented

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,19 +1,61 @@
+require 'colorize'
+
 class UsersController < ApplicationController
+
+	def index
+		log_info('UsersController >>> \'index\' action invoked')
+		@users = User.all
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
 
 	def new
 		@user = User.new
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
 	def create
 		@user = User.new(user_params)
 
-		logger.info ">> Successfully created new user"
-		@user.save
+		if @user.save
+			log_info('UsersController >>> Successfully created new user with id=' + @user.id.to_s)
+		else
+			log_error('UsersController >>> Error creating new user')
+		end
+	end
+
+	def update
+		@user = User.find(params[:id])
+
+		if @user.update(user_params)
+			log_info('UsersController >>> Successfully updated user')
+		else
+			log_error('UsersController >>> Error updating user')
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
 	end
 
 	private
+
 		def user_params
 			params.require(:user).permit(:email, :password)	
+		end
+
+		def log_error(errmsg)
+			logger.error '[ERROR]'.red + ' ' + errmsg
+		end
+
+		def log_info(msg)
+			logger.info '[INFO]'.blue + ' ' + msg
 		end
 
 end
