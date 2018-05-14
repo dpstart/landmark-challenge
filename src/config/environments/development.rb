a6d55c085cdd3a5551bdbfb1ccddd6c2eff59d5f
daniele-cappuccio <daniele97bsk@gmail.com>
2018-05-14 03:00:19 +0200

    yarnpkg integrity check removed

--- src/config/environments/development.rb
+++ src/config/environments/development.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Make javascript_pack_tag load assets from webpack-dev-server.
   config.x.webpacker[:dev_server_host] = 'http://localhost:8080'
   # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = true
+  config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
