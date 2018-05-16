c43aa129f520cd62f4814ebd8477dc338fb3ca71
daniele-cappuccio <daniele97bsk@gmail.com>
2018-05-16 20:27:41 +0200

    Devise token auth

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
 
-  devise_for :users, :controllers => {:registrations => "registrations"}
+  #devise_for :users, :controllers => {:registrations => "registrations"}
 
   get 'landing/index'
   get 'welcome/index'
 
-  resources :users
+  #resources :users
 
   root to: 'landing#index'
 end
