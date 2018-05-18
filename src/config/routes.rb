93dadda5647f51f77948e4c685d3e7c473a13c4a
daniele-cappuccio <daniele97bsk@gmail.com>
2018-05-18 03:22:46 +0200

    Added admin role to schema

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
+  mount_devise_token_auth_for 'Admin', at: 'admin/auth'
+  as :admin do
+    # Define routes for Admin within this block.
+  end
+
   #devise_for :users, :controllers => {:registrations => "registrations"}
 
   get 'landing/index'
