1228e6b2077db5ff16e15ac63a8bcf40d5261c42
daniele-cappuccio <daniele97bsk@gmail.com>
2018-05-23 15:41:58 +0200

    Redirect after signin-in with OAuth

--- src/app/controllers/custom_omniauth_callbacks_controller.rb
+++ src/app/controllers/custom_omniauth_callbacks_controller.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 class CustomOmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
 
   def omniauth_success
@@ -5,7 +7,7 @@ class CustomOmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksCont
       set_token_on_resource
       create_auth_params
 
-      logger.info '\n\nAuth success!\n\n'
+      logger.info '[INFO]'.blue + ' ' + 'Auth success!'
 
       if confirmable_enabled?
         # don't send confirmation email!!!
@@ -14,11 +16,23 @@ class CustomOmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksCont
 
       sign_in(:user, @resource, store: false, bypass: false)
 
-      @resource.save!
+      begin
+        @resource.save
+      rescue ActiveRecord::RecordNotUnique
+        # render :json => { :status => 'error', :message => 'Error in saving to database! Perhaps you previously signed up with this email? ;)' }
+        redirect_to 'http://localhost:3000/#/error/?' + 'message=' + 'error in saving to database! Perhaps you previously signed up with this email? ;)'
+        return
+      end
 
       yield @resource if block_given?
 
-      render :json => @auth_params
+      logger.info '[INFO]'.blue + ' ' + @auth_params.to_s
+
+      # render :json => @auth_params
+      redirect_to 'http://localhost:3000/#/auth_token/?' + 
+                                                         + 'token=' + @auth_params[:auth_token] +
+                                                         + '&uid=' + @auth_params[:uid] +
+                                                         + '&client_id=' + @auth_params[:client_id]
   end
 
 end
