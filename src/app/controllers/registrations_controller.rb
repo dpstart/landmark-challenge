b432ffdd19d3a27442647a94b16017fdea339632
Luca-Ferrera <luca.ferrera96@gmail.com>
2018-05-24 14:35:02 +0200

    devise_token_auth create controller overridden

--- src/app/controllers/registrations_controller.rb
+++ src/app/controllers/registrations_controller.rb
@@ -1,35 +1,70 @@
 require 'colorize'
 
-class RegistrationsController < Devise::RegistrationsController
+class RegistrationsController < DeviseTokenAuth::RegistrationsController
 
 	clear_respond_to
 	respond_to :json
 
-	def new
-		log_info("RegistrationsController > `new` action invoked")
-		super
-	end
-
+	
 	def create
-		log_info("RegistrationsController > `create` action invoked")
-
-		user = User.new(user_params)
-		user.profile = Profile.new(profile_params)
-
-		if user.profile.save
-			log_info('RegistrationsController >>> Successfully created user\'s profile with id=' + user.profile.id.to_s)
-		else
-			log_error('RegistrationsController >>> Error creating user\'s profile')
+		build_resource
+  
+		unless @resource.present?
+		  raise DeviseTokenAuth::Errors::NoResourceDefinedError,
+			"#{self.class.name} #build_resource does not define @resource, execution stopped"
 		end
+  
+		# give redirect value from params priority
+		@redirect_url = params.fetch(
+		  :confirm_success_url,
+		  DeviseTokenAuth.default_confirm_success_url
+		)
+  
+		# success redirect url is required
+		if confirmable_enabled? && !@redirect_url
+		  return render_create_error_missing_confirm_success_url
+		end
+  
+		# if whitelist is set, validate redirect_url against whitelist
+		return render_create_error_redirect_url_not_allowed if blacklisted_redirect_url?
+  
+		begin
+		  # override email confirmation, must be sent manually from ctrl
+		  resource_class.set_callback("create", :after, :send_on_create_confirmation_instructions)
+		  resource_class.skip_callback("create", :after, :send_on_create_confirmation_instructions)
+  
+		  if @resource.respond_to? :skip_confirmation_notification!
+			# Fix duplicate e-mails by disabling Devise confirmation e-mail
+			@resource.skip_confirmation_notification!
+		  end
+  
+		  @resource.profile = Profile.new(profile_params)
 
-    	if user.save
-      		render :json => {:auth_token => user.id}
-      		return
-    	else
-      		warden.custom_failure!
-      		render :json => user.errors, :status=>422
-    	end
-	end
+		  if @resource.save
+			yield @resource if block_given?
+  
+			unless @resource.confirmed?
+			  # user will require email authentication
+			  @resource.send_confirmation_instructions({
+				client_config: params[:config_name],
+				redirect_url: @redirect_url
+			  })
+			else
+			  # email auth has been bypassed, authenticate user
+			  @client_id, @token = @resource.create_token
+			  @resource.save!
+			  update_auth_header
+			end
+			render_create_success
+		  else
+			clean_up_passwords @resource
+			render_create_error
+		  end
+		rescue ActiveRecord::RecordNotUnique
+		  clean_up_passwords @resource
+		  render_create_error_email_already_exists
+		end
+  end
 
 	private
 
@@ -48,5 +83,19 @@ class RegistrationsController < Devise::RegistrationsController
 		def log_info(msg)
 			logger.info '[INFO]'.blue + ' ' + msg
 		end
-
+		def build_resource
+			@resource            = resource_class.new(sign_up_params)
+			@resource.provider   = provider
+	  
+			# honor devise configuration for case_insensitive_keys
+			if resource_class.case_insensitive_keys.include?(:email)
+			  @resource.email = sign_up_params[:email].try(:downcase)
+			else
+			  @resource.email = sign_up_params[:email]
+			end
+		 end
+		  
+		def blacklisted_redirect_url?
+			DeviseTokenAuth.redirect_whitelist && !DeviseTokenAuth::Url.whitelisted?(@redirect_url)
+	  	end
 end
