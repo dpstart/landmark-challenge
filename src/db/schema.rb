04d69089ecfd69e2cd62caa8e8bf4c605cebf997
Luca-Ferrera <luca.ferrera96@gmail.com>
2018-05-30 00:19:38 +0200

    Retriving user's achievements and goals

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2018_05_18_012042) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "profile_id"
-    t.integer "achievement"
+    t.integer "achievement_id"
   end
 
   create_table "has_visiteds", force: :cascade do |t|
