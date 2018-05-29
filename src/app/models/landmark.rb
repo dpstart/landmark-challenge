41b834bdca27852bce0044c45976cf317fd2f48c
daniele-cappuccio <daniele97bsk@gmail.com>
2018-05-29 19:38:51 +0200

    Created dataset for landmarks and cities

--- src/app/models/landmark.rb
+++ src/app/models/landmark.rb
@@ -2,6 +2,6 @@ class Landmark < ApplicationRecord
     belongs_to :city
     has_many :has_visiteds
     has_many :profiles, :through => :has_visiteds
-    validates :city, :name, uniqueness: true
+    validates :name, uniqueness: true
     validates :latitude, :longitude, :name, :city, presence: true
 end
