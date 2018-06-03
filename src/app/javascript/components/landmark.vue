3371b3c0a2301df218db3b08d53ed201c1c93666
Daniele Paliotta <danielepaliotta96@gmail.com>
2018-06-03 15:16:20 +0200

    Basic info for landmarks page

--- src/app/javascript/components/landmark.vue
+++ src/app/javascript/components/landmark.vue
@@ -3,8 +3,21 @@
     <v-content>
       <v-container fluid fill-height>
         <v-layout align-center justify-center>
-          <v-flex xs12 sm8 md4>
-            <div>{{computedData.name}}</div>
+          <v-flex xs12 sm8 md8>
+            <v-card>
+            <v-card-media  src="https://s3.amazonaws.com/iexplore_web/images/assets/000/005/495/original/dreamstime_m_9856207.jpg?1442331489" height="300px">
+            </v-card-media>
+              <v-card-title class="justify-center">
+                <h3  class="headline mb-0">{{computedData.name}}</h3>
+              </v-card-title>
+              <v-card-text class="cardtext">
+                <div>
+                  <div>{{computedData.description}}</div>
+                </div>
+              </v-card-text>
+          <v-card-actions>
+          </v-card-actions>
+          </v-card>
           </v-flex>
         </v-layout>
       </v-container>
@@ -34,5 +47,4 @@
     }
   }
 </script>
-
 
