0994d9943a9b2fade929f957291d539a30207266
Luca-Ferrera <luca.ferrera96@gmail.com>
2018-06-05 19:27:22 +0200

    Error handlers added

--- src/app/controllers/landmark_detection_controller.rb
+++ src/app/controllers/landmark_detection_controller.rb
@@ -1,10 +1,11 @@
 class LandmarkDetectionController < ApplicationController
-    # project_id = "Your Google Cloud project ID"
-    # image_path = "Path to local image file, eg. './image.png'"
+    before_action :authenticate_user!, only: [:detection]
 
     require "google/cloud/vision"
     require "open-uri"
     def detection
+        p request.headers[:uid]
+        user = User.find_by(:uid => request.headers[:uid])
         save_image()
         vision = Google::Cloud::Vision.new project: "landmark_project_204915", keyfile: "landmark-challenge-4f26e0aeca60.json"
         image  = vision.image "landmark.jpg"
@@ -17,24 +18,32 @@ class LandmarkDetectionController < ApplicationController
             @landmark_longitude = location.longitude
           end
         end
-        create_visit(@landmark_name, @landmark_latitude, @landmark_longitude,current_user.id)
+        create_visit(@landmark_name, @landmark_latitude, @landmark_longitude,user.id)
     end
     private
     def save_image
         File.open("landmark.jpg",'wb') do |f|
-            f.write params[:image].read
+            f.write params[:file].read
         end
     end
 
     def create_visit(name, lat, long, user_id)
         landmark = Landmark.find_by(:name => name)
+        if landmark == nil 
+            render :json => { :status => 'error', :message => name + " in not in our list of landmarks"}, status: 400
+            return
+        end
         landmark_id = landmark.id
         profile_id = Profile.find_by(:user_id => user_id).id
+        if HasVisited.where(:profile_id => profile_id, :landmark_id => landmark_id) 
+            render :json => { :status => 'error', :message => "You have already visited " + name }, status: 400
+            return
+        end
         @hasvisited = HasVisited.new(:landmark_id => landmark_id, :profile_id => profile_id)
         if @hasvisited.save
             render :json => { :status => 'success', :hasvisited => @hasvisited, :landmark => landmark}, status: 200
         else
-            render :json => { :status => 'error', :message => @hasvisited.errors.details}, status: 401
+            render :json => { :status => 'error', :message => @hasvisited.errors.details}, status: 400
         end
     end
 
